24cbdc705048ff98c122bb77b020f766a25ec624
wanghuan1 <wanghuan1@duiba.com.cn>
2022-04-13 23:32:01 +0800

    ts配置项处理

---
+++ built/local/tsc.d.ts
@@ -0,0 +1,6 @@
+interface Person {
+    name: string;
+    age: number;
+}
+declare const p: Person;
+//# sourceMappingURL=tsc.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+interface Person {
+    name: string
+    age:number
+}
+
+const p:Person = {
+    name:'nick',
+    age:20
+}
+
+console.log('ts!!!', p)
+
